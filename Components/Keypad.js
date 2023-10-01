a4b5e4fcf64c0b3771fc3b057154c23b15c96714
hadilkhe <121692875+hadilkhe@users.noreply.github.com>
2023-10-01 21:47:08 +0100

    Completed assignment

---
+++ Components/Keypad.js
@@ -0,0 +1,16 @@
+// components/Keypad.js
+import React from 'react';
+
+function Keypad() {
+  const handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
+}
+
+export default Keypad;
